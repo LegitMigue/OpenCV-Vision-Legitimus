5c0abccdd464958950b1ea1526160441eaf1b321
Miguel Ángel Abarca Caballero <LegitMigue@gmail.com>
2018-06-04 20:56:12 +0200

    Add more comments

--- OpenCV-Vision-Legitimus/OpenCV-Vision-Legitimus/VisionMainWindow.cpp
+++ OpenCV-Vision-Legitimus/OpenCV-Vision-Legitimus/VisionMainWindow.cpp
@@ -1,17 +1,22 @@
 #include "VisionMainWindow.h"
 
+//Main window
 VisionMainWindow::VisionMainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
 	videoPlayer = new VideoPlayer();
+	//Connect to the VideoPlayer thread signal get it (or wait for it) and send it to updatePlayerUI function
 	QObject::connect(videoPlayer, SIGNAL(processedImage(QImage)),
 		this, SLOT(updatePlayerUI(QImage)));
-
+	
+	//Start GUI
 	ui.setupUi(this);
 
+	//Start VideoPlayer thread
 	videoPlayer->Play();
 }
 
+//Recieve QImage from VideoPlayer thread and draw it in GUI video frame (keeping video frame resolution ratio)
 void VisionMainWindow::updatePlayerUI(QImage img)
 {
 	if (!img.isNull())
